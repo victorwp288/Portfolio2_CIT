33e77bdc74c96950e2cf74ce1e79ce10729562de
MadsHolt <holten06@hotmail.com>
2024-11-01 08:23:48 +0100

    Added comments

--- WebServiceLayer/Program.cs
+++ WebServiceLayer/Program.cs
@@ -4,11 +4,14 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configure Entity Framework Core to use PostgreSQL as the database provider
 builder.Services.AddDbContext<ImdbContext>(options =>
-    options.UseNpgsql("host=localhost;db=imdb;uid=postgres;pwd=Poncho24"));
+    options.UseNpgsql("host=localhost;db=imdb;uid=postgres;pwd=Ferieland128"));
 
+// Register IDataService with its implementation, DataService, using scoped lifetime
 builder.Services.AddScoped<IDataService, DataService>();
 
+// Register Mapster to handle object mapping automatically between data models and DTOs
 builder.Services.AddMapster();
 
 // Add services to the container.
